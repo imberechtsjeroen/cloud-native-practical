b9e691c429bfb9d3ffe9972559425c206fbf7717
Jeroen Imberechts <jeroen.imberechts@kbc.be>
2020-12-03 17:53:35 +0100

    lab 8

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -1,15 +1,18 @@
 package com.ezgroceries.shoppinglist;
 
+import config.ShoppingListConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableFeignClients
 @SpringBootApplication
+@Import(ShoppingListConfig.class)
 @EnableSwagger2
 public class ShoppingListApplication {
 
--- shopping-list/src/main/java/config/ShoppingListConfig.java
+++ shopping-list/src/main/java/config/ShoppingListConfig.java
@@ -1,5 +1,9 @@
 package config;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -18,6 +22,18 @@ public class ShoppingListConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    //Custom tomcat configuration, we add an additional connector that allows http traffic next to https
+    @Bean
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort) {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+
 
 
 }
